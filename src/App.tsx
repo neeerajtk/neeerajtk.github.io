d8bd8f1c49527556bd274239ebd1ad06f919ba14
neeerajtk <neerajtk560@gmail.com>
2021-01-18 00:18:55 +0530

    particles animation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Particles from "react-particles-js";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
