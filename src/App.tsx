97cbca1c361334ed83048db3f3527bdde497fb36
neeerajtk <neerajtk560@gmail.com>
2021-01-19 23:41:41 +0530

    font updated and header comp added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import ParticlesContainer from "./components/ParticlesContainer";
+import ParticlesContainer from "./components/Particles/ParticlesContainer";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <div className="particlesContainer">
+        <div className="particlesContainer">
+          <Header />
+        </div>
         <ParticlesContainer />
-        <div className="particlesContainer">Hello World</div>
       </div>
     </>
   );
