8897076c97a5b0ab01f4b554539e2e96f7fc953c
neeerajtk <neerajtk560@gmail.com>
2021-01-20 23:59:34 +0530

    styling updates

--- src/components/Body/Name.tsx
+++ src/components/Body/Name.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./style.css";
 
 export default function Name() {
-  return <div>Hi Im Neeraj</div>;
+  return (
+    <div className="nameContainer">
+      <h1 className="nameDescription">
+        Hi Im <span className="name">Neeraj</span>
+      </h1>
+    </div>
+  );
 }
