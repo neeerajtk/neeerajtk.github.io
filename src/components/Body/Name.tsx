cd8188b4374d182f76277716003a94c551c63a8b
neeerajtk <neerajtk560@gmail.com>
2021-01-21 23:12:57 +0530

    typed styling updates

--- src/components/Body/Name.tsx
+++ src/components/Body/Name.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import "./style.css";
 import Typed from "react-typed";
+import Emoji from "a11y-react-emoji";
 
 export default function Name() {
   return (
-    <div className="nameContainer">
-      <h1 className="nameDescription">
-        Hi, I'm <span className="name">Neeraj</span> !
-      </h1>
-      <Typed
-        strings={["Frontend Developer", "Btech Graduate"]}
-        typeSpeed={40}
-        backSpeed={50}
-        loop
-      />
-      <br />
-    </div>
+    <>
+      <div className="nameContainer">
+        <h1 className="nameDescription">
+          Hi, I'm <span className="name">Neeraj</span>{" "}
+          <Emoji symbol="👋🏻" label="Hi" />
+        </h1>
+      </div>
+      <div className="nameContainer">
+        <h4 className="subHeading">
+          I'm a{" "}
+          <Typed
+            className="typedText"
+            strings={[
+              "Frontend Developer !",
+              "Btech Graduate !",
+              "Minimalist !",
+            ]}
+            typeSpeed={60}
+            backSpeed={40}
+            loop
+          />
+        </h4>
+      </div>
+    </>
   );
 }
